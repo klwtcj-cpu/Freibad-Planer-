b4f99b49450f71894372499b759455e9aeda95b9
klwtcj-cpu <klwtcj@aol.com>
2026-07-06 08:46:24 +0200

    version 0.1

---
+++ app.js
@@ -0,0 +1,8 @@
+document
+.getElementById("generateButton")
+.addEventListener("click", function(){
+
+    document.getElementById("status").innerHTML =
+    "🚧 Dienstplan-Generator wird vorbereitet...";
+
+});
