bf25d5f6081f25c40f794d0bb88682d63111e96a
klwtcj-cpu <klwtcj@aol.com>
2026-07-06 13:56:41 +0200

    html vorbereitet für Dienstplan

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-document
-.getElementById("generateButton")
-.addEventListener("click", function(){
+document.getElementById("generateButton").addEventListener("click", function () {
+
+    let mitarbeiter = [];
+
+    if (document.getElementById("oli").checked) mitarbeiter.push("Oli");
+    if (document.getElementById("andi").checked) mitarbeiter.push("Andi");
+    if (document.getElementById("chiara").checked) mitarbeiter.push("Chiara");
+    if (document.getElementById("dieter").checked) mitarbeiter.push("Dieter");
 
     document.getElementById("status").innerHTML =
-    "🚧 Dienstplan-Generator wird vorbereitet...";
+        "Ausgewählt: " + mitarbeiter.join(", ");
 
 });
