53e827b92c520af1a1292a6682b599cd2b4a09fd
klwtcj-cpu <klwtcj@aol.com>
2026-07-06 14:31:40 +0200

    hinzugefügt

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 document.getElementById("generateButton").addEventListener("click", function () {
 
     let mitarbeiter = [];
+    let wochentage = [];
 
+    // Mitarbeiter
     if (document.getElementById("oli").checked) mitarbeiter.push("Oli");
     if (document.getElementById("andi").checked) mitarbeiter.push("Andi");
     if (document.getElementById("chiara").checked) mitarbeiter.push("Chiara");
     if (document.getElementById("dieter").checked) mitarbeiter.push("Dieter");
 
+    // Wochentage
+    if (document.getElementById("montag").checked) wochentage.push("Montag");
+    if (document.getElementById("dienstag").checked) wochentage.push("Dienstag");
+    if (document.getElementById("mittwoch").checked) wochentage.push("Mittwoch");
+    if (document.getElementById("donnerstag").checked) wochentage.push("Donnerstag");
+    if (document.getElementById("freitag").checked) wochentage.push("Freitag");
+    if (document.getElementById("samstag").checked) wochentage.push("Samstag");
+    if (document.getElementById("sonntag").checked) wochentage.push("Sonntag");
+
     document.getElementById("status").innerHTML =
-        "Ausgewählte Mitarbeiter: " + mitarbeiter.join(", ");
+        "<b>Mitarbeiter:</b> " + mitarbeiter.join(", ") +
+        "<br><b>Wochentage:</b> " + wochentage.join(", ");
+
 });
